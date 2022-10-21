49715afe4df0829b4b40e596fdda1d6131287580
Kanaricc <iovo7c@gmail.com>
2022-10-21 19:23:36 +0800

    chores: bump version to 1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name='chinopie',
-    version='1.2.1',
+    version='1.2.2',
     author='Kanari',
     author_email='iovo7c@gmail.com',
     description='Chino Pie is a deep learning helper.',
