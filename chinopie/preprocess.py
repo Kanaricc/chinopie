c6e4e6ae3322036ecec413fa73139abd581ea5a7
Kanaricc <iovo7c@gmail.com>
2023-02-07 13:47:24 +0800

    feat: add dual datasetwrapper

--- chinopie/preprocess.py
+++ chinopie/preprocess.py
@@ -21,3 +21,24 @@ class DatasetWrapper(Dataset):
             return invert_op()
         else:
             raise Exception("not implemented")
+
+class DatasetDualWrapper(Dataset):
+    def __init__(self,dataset:Dataset,preprocess1:Any,preprocess2:Any) -> None:
+        self.dataset=dataset
+        self.preprocess1=preprocess1
+        self.preprocess2=preprocess2
+    
+
+    def __getitem__(self, index: Any):
+        item=self.dataset.__getitem__(index)
+        item1=self.preprocess1(item)
+        item2=self.preprocess2(item)
+        
+        return item1,item2
+    
+    def __len__(self):
+        invert_op = getattr(self.dataset, "__len__", None)
+        if callable(invert_op):
+            return invert_op()
+        else:
+            raise Exception("not implemented")
