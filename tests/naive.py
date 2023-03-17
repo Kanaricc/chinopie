db7f34ab78f063b8b07da2b6563f0823c6b324f3
Kanaricc <iovo7c@gmail.com>
2023-03-17 14:48:18 +0800

    tests: naive test

--- tests/naive.py
+++ tests/naive.py
@@ -6,6 +6,7 @@ from torch.nn import functional as F
 from torch import nn
 from torch.utils.data import DataLoader
 from chinopie import TrainHelper
+from chinopie.modelhelper import TrainBootstrap
 from chinopie.datasets.fakeset import FakeRandomSet
 
 class Model(nn.Module):
@@ -17,24 +18,24 @@ class Model(nn.Module):
     def forward(self,x:Tensor):
         return self.fc1(x)
 
-def train(batch_size:int=16):
-    helper=TrainHelper('deps',10,batch_size,True,True,None,'114514',diagnose=True)
+def train(helper:TrainHelper)->float:
+    helper.reg_int('batch_size')
+    helper.reg_float('lr')
 
     trainset=FakeRandomSet(torch.zeros(10),torch.zeros(10))
     valset=FakeRandomSet(torch.zeros(10),torch.zeros(10))
     testset=FakeRandomSet(torch.zeros(10),torch.zeros(10))
-    trainloader=DataLoader(trainset,batch_size)
-    valloader=DataLoader(valset,batch_size)
-    testloader=DataLoader(testset,batch_size)
+    trainloader=DataLoader(trainset,helper.suggest_int('batch_size',1,10))
+    valloader=DataLoader(valset,helper.suggest_int('batch_size',1,10))
+    testloader=DataLoader(testset,helper.suggest_int('batch_size',1,10))
 
     model=Model()
-    optimizer=torch.optim.AdamW(model.parameters(),lr=1e-3)
+    optimizer=torch.optim.AdamW(model.parameters(),lr=helper.suggest_float('lr',1e-6,1e-1,log=True))
 
     helper.register_dataset(trainset,trainloader,valset,valloader)
     helper.register_test_dataset(testset,testloader)
 
     helper.register_probe('a')
-
     with helper.section_checkpoint_load() as s:
         pass
 
@@ -54,7 +55,7 @@ def train(batch_size:int=16):
                 loss.backward()
                 optimizer.step()
 
-                phase.update_loss(loss,batch_size)
+                phase.update_loss(loss,inputs.size(0))
             
             phase.update_probe('a',1.)
             phase.end_phase(0.)
@@ -70,7 +71,7 @@ def train(batch_size:int=16):
                 loss=F.l1_loss(outputs,targets)
                 phase.validate_loss(loss)
 
-                phase.update_loss(loss,batch_size)
+                phase.update_loss(loss,inputs.size(0))
             phase.update_probe('a',2.)
             phase.end_phase(0.)
         with helper.phase_test() as phase:
@@ -84,7 +85,7 @@ def train(batch_size:int=16):
                 loss=F.l1_loss(outputs,targets)
                 phase.validate_loss(loss)
 
-                phase.update_loss(loss,batch_size)
+                phase.update_loss(loss,inputs.size(0))
             phase.update_probe('a',3.)
             phase.end_phase(0.)
         
@@ -95,6 +96,8 @@ def train(batch_size:int=16):
             if s.should_save_best:
                 pass
 
+    return 0    
 
 if __name__=="__main__":
-    TrainHelper.auto_bind_and_run(train)
+    tb=TrainBootstrap(disk_root='deps',epoch_num=10,load_checkpoint=True,enable_checkpoint=True,comment=None)
+    tb.optimize(train,10)
