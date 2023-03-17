473f78262b53b26ef423f6fde2875fe0f7c75290
Kanaricc <iovo7c@gmail.com>
2023-03-17 18:53:05 +0800

    chore: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name='chinopie',
-    version='1.14.5',
+    version='1.15.0',
     author='Kanari',
     author_email='iovo7c@gmail.com',
     description='Chino Pie is a deep learning helper.',
