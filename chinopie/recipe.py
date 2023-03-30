b748b5a9bf07de9c500629e087f2795aea4ba6d7
Kanaricc <iovo7c@gmail.com>
2023-03-30 19:00:35 +0800

    chore: 1

--- chinopie/recipe.py
+++ chinopie/recipe.py
@@ -30,81 +30,53 @@ class ModuleRecipe:
     def testset(self):
         return self._helper._data_test,self._helper._dataloader_test
     
-    def run_phase(self,phase:str):
-        assert phase in ['train','val','test']
-        if phase=='train':self.run_train_phase(self.trainset[1])
-        elif phase=='val':self.run_val_phase(self.valset[1])
-        elif phase=='test':self.run_test_phase(self.testset[1])
-        else: raise RuntimeError(f"unknown phase `{phase}`")
-
-    
     def run_train_phase(self,dataloader:DataLoader):
         self.model.train()
         for data in dataloader:
-            self.run_iter(data,'train')
+            self.run_train_iter(data)
         pass
 
     def run_val_phase(self,dataloader:DataLoader):
         self.model.eval()
         for data in dataloader:
-            self.run_iter(data,'val')
+            self.run_val_iter(data)
         pass
 
     def run_test_phase(self,dataloader:DataLoader):
         self.model.eval()
         for data in dataloader:
-            self.run_iter(data,'test')
+            self.run_test_iter(data)
         pass
-
-    def run_iter(self,data,phase:str):
-        assert phase in ['train','val','test']
-        if phase=='train':self.run_train_iter(data)
-        elif phase=='val':self.run_val_iter(data)
-        elif phase=='test':self.run_test_iter(data)
     
     def run_train_iter(self,data:Any):
         output=self.forward(data)
         loss=self.cal_loss(data,output)
-        self.update_probe('train')
+        self.update_probe()
     
     def run_val_iter(self,data):
         with torch.no_grad():
             output=self.forward(data)
             loss=self.cal_loss(data,output)
-            self.update_probe('val')
     
     def run_test_iter(self,data):
         with torch.no_grad():
             output=self.forward(data)
             loss=self.cal_loss(data,output)
-            self.update_probe('test')
     
     def forward(self,data)->Tensor:
-        ...
+        raise NotImplemented
     
     def cal_loss(self,data,output:Tensor)->Tensor:
-        ...
+        raise NotImplemented
     
-    def update_probe(self,probes,output:Tensor,phase:str):
-        assert phase in ['train','val','test']
-        if phase=='train':self.update_train_probe(data)
-        elif phase=='val':self.update_val_probe(data)
-        elif phase=='test':self.update_test_probe(data)
-    
-    def update_train_probe(self,probes,output):
-        ...
-    
-    def update_val_probe(self,probes,output):
-        ...
-    
-    def update_test_probe(self,probes,output):
-        ...
+    def update_probe(self):
+        raise NotImplemented
 
     def restore_ckpt(self,ckpt):
-        ...
+        raise NotImplemented
     
     def export_ckpt(self):
-        ...
+        raise NotImplemented
     
     def fit(self)->float|Sequence[float]:
         for epochi in self._helper.range_epoch():
